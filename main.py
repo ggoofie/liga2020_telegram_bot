69a0deb1189d7195fb4900a28f2ee6187226f8db
ggoofie <farsiyan.vartan@yandex.ru>
2020-02-08 20:11:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +10,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
