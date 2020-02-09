1cdc2fe0a171791e5a6a9ac0985b0a9448f8d30c
ggoofie <farsiyan.vartan@yandex.ru>
2020-02-09 16:40:12 +0300

    Go to aiogramm

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
+import logging
 import os
-from flask import Flask
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
 
-app = Flask(__name__)
+API_TOKEN = os.environ.get('API_TOKEN')
+
+# webhook settings
+WEBHOOK_URL = f"{os.environ.get('HEROKU_URL')}{API_TOKEN}"
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.environ.get('PORT'))
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
-@app.route('/')
-def index():
-    return '<h1>Test Flask app<h1>'
+@dp.message_handler()
+async def echo(message: types.Message):
+    # Regular request
+    # await bot.send_message(message.chat.id, message.text)
+
+    # or reply INTO webhook
+    return SendMessage(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
